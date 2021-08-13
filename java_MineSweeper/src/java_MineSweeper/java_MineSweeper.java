da512ecc347fd86366f4c1e911ebe0f4dbfe3e7b
KIMHYEIN <ohio0510@naver.com>
2021-08-13 17:59:59 +0900

    init commit

---
+++ java_MineSweeper/src/java_MineSweeper/java_MineSweeper.java
@@ -0,0 +1,29 @@
+package java_MineSweeper;
+import java.util.*;
+
+public class java_MineSweeper {
+
+	public static void main(String[] args) {
+		int[][ ] board =  new int[10][10];
+		
+		for(int i=0; i<10; i++) {
+			for(int j =0; j<10; j++) {
+				if(Math.random() < 0.3)
+					board[i][j]=1;
+			}
+		}
+		for(int i =0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				if(board[i][j]==1) {
+					System.out.print("# ");
+				}
+				
+				else
+					System.out.print(". ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
